f31a6a99baed4da505952373a3c7f0acc8b141b7
Elias Abril <elias.abril.f@gmail.com>
2022-10-14 11:35:12 +0100

    update

--- project/project.py
+++ project/project.py
@@ -6,9 +6,8 @@ import re
 
 def main():
     #url = f"https://youtu.be/{userIn()}"
-    yt = youtube("https://www.youtube.com/watch?v=V2KU5HracvI&ab_channel=LoopDaddyFanPage")
+    yt = youtube("https://www.youtube.com/watch?v=eQ_8F4nzyiw&ab_channel=LinusTechTips")
     
-
 
 def userIn():
     while True:
@@ -24,20 +23,34 @@ def userIn():
             
 def youtube(url):
     yt = YouTube(url)
-    video = []
-    audio = []
     video = f"{yt.streams.filter(only_video=True)}".split(", ")
-    #audio = f"{yt.streams.filter(only_audio=True)}".split(", ")
+    audio = f"{yt.streams.filter(only_audio=True)}".split(", ")
+    vid = extract_video(video)
+    print(vid)
+    aud = extract_audio(audio)
     
+    
+    
+def extract_video(video):
+    streams = {}
     for i in video:
-        print(f'ORIGINAL STREAM \n{i} \n')
-        # <Stream: itag="134" mime_type="video/mp4" res="360p" fps="30fps" vcodec="avc1.4d401e" progressive="False" type="video">
-        vid = re.search(r'^\[?<Stream: itag="(.{3})" mime_type="video/(?:webm|mp4)" res="(1080p|1440p|2160p)" fps="30fps" vcodec="(?:vp9|avc1.(?:4d401f|640028))" progressive="False" type="video">\]?$', i, re.IGNORECASE)
+        vid = re.search(r'^\[?<Stream: itag="(.{3})" mime_type="video/(?:webm|mp4)" res="(1080|1440|2160)p" fps="30fps" vcodec="(?:vp9|avc1.(?:4d401f|640028))" progressive="False" type="video">\]?$', i, re.IGNORECASE)
         try:
             if vid[2]:
-                                print(f'CAPTURES \n{vid[1]} {vid[2]}')
+                streams[vid[1]] = vid[2]
         except:
             continue
+        
+    return max(streams)
+    
+def extract_audio(audio):
+    streams = {}
+    for i in audio:
+        print(i)
+        aud = re.search = r'^\[?<Stream: itag="(.{3})" mime_type="audio/(?:mp4|webm)" abr="(128|160)kbps" acodec="(?:opus|mp4a.40.(?:2|5))" progressive="False" type="audio">\]?$' 
+        print(aud)
+        
+    return max(streams)
 if __name__ == "__main__":
     main()
     
