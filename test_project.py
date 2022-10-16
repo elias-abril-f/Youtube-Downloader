ff3953a448c5cf3f5d0303b188192fe3b0d3f627
Elias-Odonnell <elias.abril.f@gmail.com>
2022-10-16 17:34:19 +0000

    16 Oct 2022, 18:34

--- test_project.py
+++ test_project.py
@@ -1,6 +1,7 @@
-from project import combine, userIn, youtube, extract_max
-
-
+from project import combine, userIn, youtube, extract_max, main
+from pytube import YouTube
+    
+    
 def test_combine():
     
     assert combine("John Mayer - Ain't no Sunshine", "webm", "mp4") == "Cool beans"
@@ -9,4 +10,11 @@ def test_userIn():
     assert userIn("https://www.youtube.com/watch?v=CwAnLe4R4F0&t=1022s&ab_channel=Goosee9") == "CwAnLe4R4F0"
     
 def test_youtube():
-    assert youtube("CwAnLe4R4F0") == "David Ryan Harris feat. John Mayer and Friends - YSD Jam Session - Hotel Café - 2/4/2020 4K Video", "", 
+    assert youtube("CwAnLe4R4F0") == "David Ryan Harris feat. John Mayer and Friends - YSD Jam Session - Hotel Café - 2/4/2020 4K Video"; "mp4"; "webm"
+    
+def test_extract_max():
+    yt = YouTube("CwAnLe4R4F0")
+    video = f"{yt.streams.filter(only_video=True)}".split(", ")
+    audio = f"{yt.streams.filter(only_audio=True)}".split(", ")
+    assert extract_max(video) == "401"; "webm"
+    assert extract_max(audio) == "251"; "mp4"
